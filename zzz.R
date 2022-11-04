bd1fed6a7dad2a870e209cd94561efb21fda5e8b
Philipp Baumann <baumann-philipp@proton.me>
2022-11-04 04:35:17 +0100

    add more py pkgs via .onLoad

--- zzz.R
+++ zzz.R
@@ -1,7 +1,9 @@
-# global reference to scipy (will be initialized in .onLoad)
-xarray <- NULL
+# global reference to python pkgs (will be initialized in .onLoad)
+xarray <- watchdog <- zarr <- NULL
 
 .onLoad <- function(libname, pkgname) {
   # use superassignment to update global reference to scipy
   xarray <<- reticulate::import("xarray", delay_load = TRUE)
+  watchdog <<- reticulate::import("watchdog", delay_load = TRUE)
+  zarr <<- reticulate::import("zarr", delay_load = TRUE)
 }
